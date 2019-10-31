39f11dd2372604994decf5d5038b6722b9f5e7ae
Ngonidzashe Choga <ngonidzashe.choga@uct.ac.za>
2019-10-31 08:57:35 +0200

    OPENCAST-1441 - Added save file for opencast

--- detectVoiceInWave.py
+++ detectVoiceInWave.py
@@ -4,6 +4,12 @@ import argparse
 from config.logging_setup import logger
 
 
+def save_to_file(empty, filename):
+    f = open(filename, 'w')
+    f.write('empty={}\n'.format(empty))
+    f.close()
+
+
 def get_speech_duration(speech):
     duration = 0
     for segment in speech:
@@ -11,19 +17,21 @@ def get_speech_duration(speech):
     return duration
 
 
-def detect_speech(input_file):
+def detect_speech(input_file, output_file):
     v = VoiceActivityDetector(input_file)
     raw_detection = v.detect_speech()
     speech_labels = v.convert_windows_to_readible_labels(raw_detection)
     speech = get_speech_duration(speech_labels)
     logger.info("File: {}, Duration: {}".format(input_file, speech))
     empty_venue = (v.duration / speech if speech > 0 else 1) > 0.95
+    save_to_file(empty_venue, output_file)
     logger.info("File: {}, Empty: {}".format(input_file, empty_venue))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=ARG_DESCRIPTION)
     parser.add_argument('-i', '--input', dest='inputfile', metavar='(wav)', help=INPUT_DESCRIPTION)
+    parser.add_argument('-o', '--output', dest='outputfile', metavar='(txt)', help=OUTPUT_DESCRIPTION)
     args = parser.parse_args()
 
-    detect_speech(args.inputfile)
+    detect_speech(args.inputfile, args.outputfile)
