3b0bca699891dbd5908e4310525515cb9d5803a2
Ngonidzashe Choga <ngonidzashe.choga@uct.ac.za>
2019-10-31 11:03:19 +0200

    OPENCAST-1441 - Log address in config file

--- config/logging_setup.py
+++ config/logging_setup.py
@@ -1,10 +1,11 @@
 import logging
+from .config import LOGS
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 log_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 
-file_handler = logging.FileHandler('logs/logs.log')
+file_handler = logging.FileHandler(LOGS)
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(log_format)
 logger.addHandler(file_handler)
