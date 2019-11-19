c62f09428672491f3ba50932eb15b99fbaa95c8a
Ngonidzashe Choga <ngonidzashe.choga@uct.ac.za>
2019-11-19 09:47:40 +0200

    OPENCAST-1441 - changed logic, script to go fetch data from db

--- detectVoiceInWave.py
+++ detectVoiceInWave.py
@@ -1,6 +1,8 @@
 from vad import VoiceActivityDetector
+from flac_to_wav import flac_to_wav
 from descriptions import *
 import argparse
+import os
 from config.logging_setup import logger
 
 
@@ -17,17 +19,52 @@ def get_speech_duration(speech):
     return duration
 
 
-def detect_speech(input_file, output_file):
+def get_media_file(media_id):
+    media_location = "/data/opencast/archive/mh_default_org/{}".format(media_id)
+
+    most_up_to_date_media_folder = ""
+    flac_name = ""
+    file_list = []
+
+    for dirName, subdirList, fileList in os.walk(media_location):
+        most_up_to_date_media_folder = dirName
+        file_list = fileList
+
+    for file_name in file_list:
+        if ".flac" in file_name:
+            flac_name = file_name
+
+    return most_up_to_date_media_folder, flac_name
+
+
+def check_or_create_tmp_folder():
+    directory = "temp"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def detect_speech(media_id, output_file):
     try:
+        check_or_create_tmp_folder()
+
+        most_up_to_date_media_folder, flac_name = get_media_file(media_id)
+        logger.info("Fetching flac file from: {}, name of file is: {}".format(most_up_to_date_media_folder, flac_name))
+        input_file = flac_to_wav(most_up_to_date_media_folder + "/" + flac_name)
+        logger.info("Converted media file is located at: {}".format(input_file))
+
         v = VoiceActivityDetector(input_file)
         raw_detection = v.detect_speech()
         logger.info("Speech detection complete")
+
         speech_labels = v.convert_windows_to_readible_labels(raw_detection)
         speech = get_speech_duration(speech_labels)
         logger.info("File: {}, Duration: {}".format(input_file, speech))
         empty_venue = (speech / v.duration) < 0.11
         save_to_file(empty_venue, output_file)
         logger.info("File: {}, Empty: {}".format(input_file, empty_venue))
+
+        os.remove(input_file)
+        logger.info("Converted media {} has been deleted".format(input_file))
     except Exception as e:
         logger.error("Something went wrong while trying to detect speech: {}".format(e))
 
