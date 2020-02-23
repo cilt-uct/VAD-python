b1bb57fa2eadb0a607e441d38e01c7c343616bc6
Stephen Marquard <stephen.marquard@uct.ac.za>
2020-02-23 16:26:46 +0200

    added address in file to ease deployment

--- config/logging_setup.py
+++ config/logging_setup.py
@@ -1,11 +1,10 @@
 import logging
-from .config import LOGS
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-file_handler = logging.FileHandler(LOGS)
+file_handler = logging.FileHandler("/opt/VAD-python/logs/logs.log")
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(log_format)
 logger.addHandler(file_handler)
