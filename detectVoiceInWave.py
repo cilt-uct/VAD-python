373424057fadfa92f83ccb2898d80dfdd0be2b95
Ngonidzashe Choga <ngonidzashe.choga@uct.ac.za>
2020-02-26 15:20:59 +0200

    OPENCAST-1441 - Updated logging

--- detectVoiceInWave.py
+++ detectVoiceInWave.py
@@ -24,7 +24,7 @@ def detect_speech(input_file, output_file="results.txt"):
         logger.info("Speech detection complete")
         speech_labels = v.convert_windows_to_readible_labels(raw_detection)
         speech = get_speech_duration(speech_labels)
-        logger.info("File: {}, Duration: {}".format(input_file, speech))
+        logger.info("File: {}, Duration of video: {}, Duration of speech: {}".format(input_file, v.duration, speech))
         empty_venue = (speech / v.duration) < 0.05
         save_to_file(empty_venue, output_file)
         logger.info("File: {}, Empty: {}".format(input_file, empty_venue))
